82cad783a6fe03852b92987712e3e0ddee6934ab
Carlos A. Gómez Urda <carlosurda@yahoo.es>
2015-11-14 19:31:23 +0100

    Ejercicio 1 (Word Play) completado en la semana 1 del curso 3.

--- Programacion en Java: Arrays, Listas y Estructuras de Datos/Semana 1/WordPlay/WordPlay.java
+++ Programacion en Java: Arrays, Listas y Estructuras de Datos/Semana 1/WordPlay/WordPlay.java
@@ -1,6 +1,6 @@
 
 /**
- * Ejercicio 1 de la Semana 1 del curso «Programacion en Java: Arrays, Listas y Estructuras de Datos»
+ * Ejercicio 1 de la Semana 1 del curso «Programacion en Java: Arrays, Listas y Estructuras de Datos».
  * 
  * @author Carlos A. Gómez Urda
  * @version 1.0
@@ -22,8 +22,86 @@ public class WordPlay
     }
     
     
+    /**
+     * Sustituir las vocales de una cadena por un carácter determinado.
+     * 
+     * @param phrase Cadena en la cual realizar la sustitución.
+     * @param ch Carácter a sustituir.
+     * @return Cadena con las vocales sustituidas. Null si hay algún error.
+     */
+    private String replaceVowels( String phrase, char ch)
+    {
+        if (phrase == null) return null;
+        
+        StringBuilder cadena = new StringBuilder( phrase);
+        int longitud = cadena.length();
+        
+        for (int i = 0; i < longitud; i++)
+            if (isVowel( cadena.charAt( i))) cadena.setCharAt( i, ch);
+        
+        return cadena.toString();
+    }
+    
+    
+    /**
+     * Sustituir en una cadena un carácter determinado por '*' si está en posición impar o '+' si
+     * está en posición par.
+     * 
+     * @param phrase Cadena en la cual realizar la sustitución.
+     * @param ch Carácter a sustituir (ya esté en mayus o minus).
+     * @return Cadena con las vocales sustituidas. Null si hay algún error.
+     */
+    private String emphasize( String phrase, char ch)
+    {
+        if (phrase == null) return null;
+        
+        StringBuilder cadena = new StringBuilder( phrase);
+        int longitud = cadena.length();
+        
+        ch = Character.toLowerCase( ch);
+        
+        for (int i = 0; i < longitud; i++)
+        {
+            if (ch != Character.toLowerCase( cadena.charAt( i))) continue;
+            
+            cadena.setCharAt( i, (i % 2) == 0 ? '*' : '+');            
+        }
+                    
+        return cadena.toString();
+    }
+
+    
+    /**
+     * Probar la función isVowel
+     * 
+     * @param ch Carácter a comprobar
+     */
     public void testIsVowel( char ch)
     {
-        System.out.println( "Carácter " + ch + ": " + (isVowel( ch) ? "Verdad" : "Falso"));
+        String esVocal = isVowel( ch) ? "Verdad" : "Falso";
+        System.out.println( "Carácter " + ch + ": " + esVocal);
     }
+    
+    /**
+     * Probar la función replaceVowels
+     * 
+     * @param cadena Cadena a sustituir las vocales por un carácter
+     * @param ch Carácter a sustituir.
+     */
+    public void testReplaceVowel( String cadena, char ch)
+    {
+        System.out.println( cadena + " (" + ch + ") => " + replaceVowels( cadena, ch));
+    }
+    
+    /**
+     * Probar la función emphasize.
+     * 
+     * @param phrase Cadena en la cual realizar la sustitución.
+     * @param ch Carácter a sustituir (ya esté en mayus o minus).
+     */
+    public void testEmphasize( String cadena, char ch)
+    {
+        System.out.println( cadena + " (" + ch + ") => " + emphasize( cadena, ch));
+    }
+    
 }
